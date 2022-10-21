a0bc35651a873d555e6f0eeb7d1941bfe8c3ba90
gamagegit <cgamage92@yahoo.com>
2022-10-21 10:23:26 -0700

    implemented complete search function

--- babyNameTrends/src/NameFrame.java
+++ babyNameTrends/src/NameFrame.java
@@ -10,8 +10,14 @@ import java.io.*;
  the NameComponent, deals with the controls and messaging the
  NameComponent.
 */
-class NameFrame extends JFrame 
+class NameFrame extends JFrame implements ActionListener
 {
+	// create swing components 
+	private JButton cancel;
+	private JButton ok;
+	private JTextField searchBox; 
+	
+	
 	// Controls if main() does a doSearch()
 	public static final boolean SEARCH = true;
 	
@@ -19,50 +25,55 @@ class NameFrame extends JFrame
 	public static ArrayList <NameRecord> ivar = new ArrayList<NameRecord>();
 	
 	// initiate string variable to hold search inquery
-	public static String target ="sam";
+	public static String target ="";
+	
+	
+	
+	
 	
 	public static void main(String[] args) throws IOException {
 		NameFrame frame = new NameFrame();
-		
-		
-		// creating temporary search window
-		
-		frame.setTitle("Input");
-		frame.setSize(300, 150);
-		frame.setLayout(null);
-		
-		// add label  and search field
-		JLabel label = new JLabel("Search Names");
-		label.setBounds(50,10,100,20);
-		JTextField textField=new JTextField("");
-		textField.setBounds(50,40,200,20);
-		
-		// add buttons
-		JButton cancel=new JButton("Cancel");
-		cancel.setBounds(50,80,80,30);  
-		JButton ok=new JButton("Ok");
-		ok.setBounds(150,80,80,30);
-		
-		
-		// set button actions
-		//TODO
-		
-		
-		frame.add(cancel);
-		frame.add(ok);
-		frame.add(label);
-		frame.add(textField);
-		frame.setVisible(true);
-		
-		
-
 		frame.read("names-data.txt");
 		
-		
+		// calls do search method if search is true
 		if (SEARCH) {
 			frame.doSearch();
 		}					
 	}
+	
+	
+	//displays search window
+	public void showInputDialog() {		
+		
+		setTitle("Input");
+		setSize(300, 150);
+		setLayout(null);
+		
+		// add label  and search field
+		JLabel label = new JLabel("Search Names");
+		label.setBounds(50,10,100,20);
+		searchBox=new JTextField("");
+		searchBox.setBounds(50,40,200,20);
+		
+		// add buttons
+		cancel=new JButton("Cancel");
+		cancel.setBounds(50,80,80,30);  
+		ok=new JButton("Ok");
+		ok.setBounds(150,80,80,30);
+		
+		
+		// add action listners
+		cancel.addActionListener(this);
+		ok.addActionListener(this);
+		
+
+		// add elements to searchBox
+		add(cancel);
+		add(ok);
+		add(label);
+		add(searchBox);
+		setVisible(true);
+	}	
 	// read method- iterate through given file/ return exceptions.
 	private void read(String fileName) throws IOException {	
 		
@@ -88,29 +99,57 @@ class NameFrame extends JFrame
 	}
 	
 	
-	// 
-	private ArrayList<String> search(String target) {
+	// Perform search according for target value
+	private void search(String target) {
+		
+		// Arraylist holds search results.
 		ArrayList <String> searchResult = new ArrayList<String>();
+		
+		// iterates over the arraylist generated from read function.
 		for( int i =0; i< ivar.size(); i++) {
 			if(ivar.get(i).getName().toLowerCase().contains(target.toLowerCase())) {
+				
+				// create a string with name and best year, append it to search results.
 				String temp =  ivar.get(i).getName() + " " + ivar.get(i).bestYear();
 				searchResult.add(temp);
 			}
 		}
 		
 		
-		return searchResult;
+		// this indicates the start of search results
+		System.out.println("\n\n*** Search Results *** \n");
 		
-		
-	}
-	private void doSearch() {
-		
-		ArrayList <String> results =search(target);
-		
-		for( int i = 0 ; i< results.size(); i++) {
-			System.out.println(results.get(i));
+		// iterate over search results and print out the results.
+		for( int i = 0 ; i< searchResult.size(); i++) {
+			System.out.println(searchResult.get(i));
 		}
+		// this indicates the end of search results
+		System.out.println("\n\n*** End of search results *** \n");
 	}
+	
+	// calls display function of search box
+	private void doSearch() {
+		showInputDialog();
+		
+	}
+	
+	
+	// handles button events of search box
+	// calls search functions after each event to print up to date search results.
+	public void actionPerformed( ActionEvent e) {
+		if(e.getSource().equals(ok)){
+			target = searchBox.getText();
+			search(target);
+		}else if(e.getSource().equals(cancel)) {
+			
+			// on click of cancel, returns all available data without search.
+			target = "";
+			searchBox.setText("");
+			search(target);
+		}
+		
+	}
+		
 	
 	
 	
